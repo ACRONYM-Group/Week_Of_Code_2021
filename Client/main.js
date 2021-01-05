138884ce8f152b0cc07ee981c46dcb984f48a667
23156436 <jordanayan@live.com>
2021-01-04 22:30:03 -0600

    Load grid from ACI

--- Client/main.js
+++ Client/main.js
@@ -1,4 +1,21 @@
-grid = [];
+function onConnected() {
+
+}
+
+function onMessage(data) {
+    if (data["key"] == "map" && data["db_key"] == "gamedata") {
+        console.log("Got Grid");
+        grid = data["val"];
+        var d = new Date();
+        var n = d.getTime();
+        console.log(n);
+    }
+}
+
+ACIConnection = new connection("scienceandpizza.com", 8766, onConnected, onMessage);
+ACIConnection.start();
+
+var grid = [];
 tile_types = [{"name":"grass", "color":"#009900", "blocks":false, "spawn_chance":0.9},{"name":"stone", "color":"#999999", "tiles":false, "spawn_chance":0.3},{"name":"wall", "color":"#8f6d0e", "block":true, "spawn_chance":0.05}]
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -15,6 +32,7 @@ class tile {
 function generate_grid(width, height) {
     for (var x= 0; x < width; x++) {
         grid[x] = []
+        console.log(x);
         for (var y = 0; y < height; y++) {
             choosen_type = 0;
             choosen_type_priority = 0;
@@ -46,7 +64,7 @@ function generate_grid(width, height) {
 
 function draw_grid() {
     //tile_size = canvas.clientWidth/grid.length;
-    tile_size = 30;
+    tile_size = 15;
     for (var x = 0; x < grid.length; x++) {
         for (var y = 0; y < grid[x].length; y++) {
             ctx.fillStyle = grid[x][y].color;
@@ -61,5 +79,16 @@ canvas.height = window.innerHeight;
 ctx = canvas.getContext("2d");
 ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
 
-generate_grid(100, 50);
-draw_grid();
+
+setTimeout(function() {
+    ACIConnection.a_authenticate("bots.woc_2021", "AbDc314")
+}, 500);
+    
+setTimeout(function() {
+    ACIConnection.getRequest("map", "gamedata");
+    var d = new Date();
+    var n = d.getTime();
+    console.log(n);
+}, 1000);
+
+setInterval(draw_grid, 500);

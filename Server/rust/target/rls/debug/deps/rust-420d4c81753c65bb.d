b58f42aae07b679b458626cd3d7843006b855915
CarterTS <carterplasek@gmail.com>
2021-01-04 22:39:56 -0600

    Remove target directory

--- Server/rust/target/rls/debug/deps/rust-420d4c81753c65bb.d
+++
@@ -1,5 +0,0 @@
-c:\Users\ethan.ETHAN-DESKTOP\OneDrive\Documents\CodeSpace\AWOC2021\Week_Of_Code_2021\Server\rust\target\rls\debug\deps\rust-420d4c81753c65bb.rmeta: src\main.rs
-
-c:\Users\ethan.ETHAN-DESKTOP\OneDrive\Documents\CodeSpace\AWOC2021\Week_Of_Code_2021\Server\rust\target\rls\debug\deps\rust-420d4c81753c65bb.d: src\main.rs
-
-src\main.rs:

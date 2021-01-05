9f79f28f4f851b352acc04144d58fc06f0b87fc9
23156436 <jordanayan@live.com>
2021-01-04 21:37:41 -0600

    Wall Generation

--- Client/main.js
+++ Client/main.js
@@ -1,24 +1,45 @@
 grid = [];
-block_types = [{"name":"grass", "color":"#009900"},{"name":"stone", "color":"#999999"}]
+tile_types = [{"name":"grass", "color":"#009900", "blocks":false, "spawn_chance":0.9},{"name":"stone", "color":"#999999", "tiles":false, "spawn_chance":0.3},{"name":"wall", "color":"#8f6d0e", "block":true, "spawn_chance":0.05}]
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
 
 class tile {
-    constructor(type, color) {
-        this.type = type;
-        this.color = color;
+    constructor(type) {
+        this.type = tile_types[type].name;
+        this.color = tile_types[type].color;
+        this.blocks = tile_types[type].blocks;
     }
 }
 
 function generate_grid(width, height) {
-    for (var x = 0; x < width; x++) {
+    for (var x= 0; x < width; x++) {
         grid[x] = []
         for (var y = 0; y < height; y++) {
-            block_type = block_types[getRandomInt(block_types.length)].name;
-            block_color = block_types[getRandomInt(block_types.length)].color;
-            console.log(block_type);
-            grid[x][y] = new tile(block_type, block_color);
+            choosen_type = 0;
+            choosen_type_priority = 0;
+            for (tile_type in tile_types) {
+                tile_type_priority = getRandomInt(100);
+                if (tile_type_priority*tile_types[tile_type].spawn_chance > choosen_type_priority) {
+                    choosen_type = tile_type;
+                    choosen_type_priority = tile_type_priority;
+                }
+            }
+            try {
+                if (grid[x-1][y].type == "stone" || grid[x][y-1].type == "stone") {
+                    if (getRandomInt(10) > 3) {
+                        choosen_type = 1
+                    }
+                }
+                if ((grid[x-1][y].type == "wall" || grid[x][y-1].type == "wall") && !(grid[x-1][y].type == "wall" && grid[x][y-1].type == "wall")) {
+                    if (getRandomInt(10) > 2) {
+                        choosen_type = 2
+                    }
+                }
+            } catch (e) {
+
+            }
+            grid[x][y] = new tile(choosen_type);
         }
     }
 }

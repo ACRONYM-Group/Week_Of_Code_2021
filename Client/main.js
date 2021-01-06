09f4279e0c29ca0f50fe203b8197a5dfea8343cf
Ethan <ethandanielhunt@gmail.com>
2021-01-06 00:03:19 -0500

    dual arrow controls and wasd controls

--- Client/main.js
+++ Client/main.js
@@ -77,16 +77,16 @@ function updateGameArea() {
 	myGameArea.clear();
 	myGamePiece.moveAngle = 0;
 	myGamePiece.speed = 0;
-	if (myGameArea.keys && myGameArea.keys[65]) {
+	if (myGameArea.keys && (myGameArea.keys[65] || myGameArea.keys[37])) {
 		myGamePiece.moveAngle = -1;
 	}
-	if (myGameArea.keys && myGameArea.keys[68]) {
+	if (myGameArea.keys && (myGameArea.keys[68] || myGameArea.keys[39])) {
 		myGamePiece.moveAngle = 1;
 	}
-	if (myGameArea.keys && myGameArea.keys[87]) {
+	if (myGameArea.keys && (myGameArea.keys[87] || myGameArea.keys[38])) {
 		myGamePiece.speed = 1;
 	}
-	if (myGameArea.keys && myGameArea.keys[83]) {
+	if (myGameArea.keys && (myGameArea.keys[83] || myGameArea.keys[40])) {
 		myGamePiece.speed = -1;
 	}
 	myGamePiece.newPos();
